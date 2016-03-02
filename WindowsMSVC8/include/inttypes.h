c95b13ec19cc1c4d0ea71be1324797a271b7f5c1
Heiko Rabe <hrabe@LEIWS012.invision.global>
2016-03-02 17:15:04 +0100

    Add replacement files for non existing files at MSVC compilers
    - created inttypes.h with required definitions
    - created strings.h with required definitions
    - created empty stdint.h for qualified compile

---
+++ WindowsMSVC8/include/inttypes.h
@@ -0,0 +1,40 @@
+#ifndef __INTTYPES_H__
+
+#define __INTTYPES_H__
+
+#include <math.h>
+#include <limits.h>
+
+#if defined(_MSC_VER) 
+typedef signed __int8 int8_t; 
+typedef signed __int16 int16_t; 
+typedef signed __int32 int32_t; 
+typedef signed __int64 int64_t; 
+typedef unsigned __int8 uint8_t; 
+typedef unsigned __int16 uint16_t; 
+typedef unsigned __int32 uint32_t; 
+typedef unsigned __int64 uint64_t; 
+
+#define __attribute__(A) /* do nothing */
+
+#ifdef  _WIN64
+typedef unsigned __int64    ssize_t;
+#define _UI64_MAX SSIZE_MAX;
+#else
+typedef _W64 unsigned int   ssize_t;
+#define SSIZE_MAX _UI64_MAX
+#endif //_WIN64
+
+#define PRIu64 "I64u"
+#define PRId64 "I64d"
+
+// Calculates log2 of number.  
+static double log2( double n )  
+{  
+    // log(n)/log(2) is log2.  
+    return log( (double)n ) / log( (double)2 );  
+}
+
+#endif //_MSC_VER
+
+#endif //__INTTYPES_H__
---
+++ WindowsMSVC8/include/strings.h
@@ -0,0 +1,8 @@
+#ifndef __STRINGS_H__
+
+#define __STRINGS_H__
+
+#define strncasecmp _strnicmp
+#define strcasecmp _stricmp
+
+#endif //__STRINGS_H__
